3e5d80dc3d567131ee85900f071bcb660fe66ddc
Dillon Lewis <dillon.lewis@anywhere.re>
2024-08-15 13:30:28 -0400

    Implement recommending posts by email feature for blog platform.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.db.models.functions import Now
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -20,7 +21,10 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(
+        max_length=250,
+        unique_for_date='publish'
+        )
     body = models.TextField()
     publish = models.DateTimeField(db_default=Now())
     created = models.DateTimeField(auto_now_add=True)
@@ -37,3 +41,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail',
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+            ]
+        )
