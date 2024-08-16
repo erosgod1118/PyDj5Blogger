18dddbc1f7e5b02bc631caa1acabf9666267eef5
Dillon Lewis <dillon.lewis@anywhere.re>
2024-08-16 04:08:09 -0400

    Implement adding comment system.

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,12 @@ from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.conf import settings
+from django.views.decorators.http import require_POST
 import smtplib
 import ssl
 
 from .models import Post
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 def post_list(request):
     post_list = Post.published.all()
@@ -36,10 +37,18 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day
     )
+
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+
     return render(
         request,
         'blog/post/detail.html',
-        {'post': post}
+        {
+            'post': post,
+            'comments': comments,
+            'form': form
+        }
     )
 
 class PostListView(ListView):
@@ -92,5 +101,29 @@ def post_share(request, post_id):
             'post': post,
             'form': form,
             'sent': sent
+        }
+    )
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment
         }
     )
