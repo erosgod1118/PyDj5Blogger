db53767f856b1689f874f7f191c251c54cbe4f9b
Eros God <erosgod19921118@gmail.com>
2024-08-23 16:41:32 -0400

    Add infinite scroll pagination to the image list.

--- BookmarkPlatform/images/views.py
+++ BookmarkPlatform/images/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.http import require_POST
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Image
 from .forms import ImageCreateForm
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    print("Image Users Like Cnt: ", image.users_like.count())
     return render(
         request,
         'images/image/detail.html',
@@ -36,4 +40,54 @@ def image_create(request):
         request,
         'images/image/create.html',
         {'section': 'images', 'form': form},
+    )
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
+
+    return JsonResponse({'status': 'error'})
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            return HttpResponse('')
+        # If page out of range return last page of results
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(
+            request,
+            'images/image/list_images.html',
+            {'section': 'images', 'images': images}
+        )
+    
+    return render(
+        request,
+        'images/image/list.html',
+        {'section': 'images', 'images': images}
     )
